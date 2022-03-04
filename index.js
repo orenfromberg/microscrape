3d91ac5c9f7963eff7efcbb01356e16fe2ec3901
Oren Fromberg <oren@adhocteam.us>
2022-03-04 12:37:00 -0500

    Fixes

--- index.js
+++ index.js
@@ -5,12 +5,12 @@ const {
 } = require('./microcenter');
 const axios = require('axios');
 
-const get_inventory = url => {
+const check_inventory = url => {
     return fetch_inventory(axios, url, store_codes["Brentwood, MO"])
 }
 
-const promises = products_urls.map(get_inventory)
+// const promises = products_urls.map(check_inventory)
 
-Promise.all(promises).then(values => console.log(values))
+// Promise.all(promises).then(values => console.log(values))
 
-// get_inventory(products_urls[0]).then(values => console.log(values));
+check_inventory(products_urls[0]).then(values => console.log(values));
